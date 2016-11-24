6a2f609fad04c12b30e90e52a1df2ff15dd062f2
haizimen <694931318@qq.com>
2016-11-24 17:25:42 +0800

    diyicitijiao

---
+++ app/src/main/java/com/hezi/advanceddaygit/MainActivity.java
@@ -0,0 +1,19 @@
+package com.hezi.advanceddaygit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+
+    }
+}
